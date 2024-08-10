84d216aba935520749bda365f3d3316c8afd6141
bhaskar359 <bhaskarnarendra359@gmail.com>
2024-08-10 07:08:41 +0530

    v1.2

--- app/Main.tsx
+++ app/Main.tsx
@@ -20,8 +20,8 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hey there! WELCOME TO MY 6LOG,
+          <h1 className="bg-gradient-to-r from-orange-600 to-orange-400 bg-clip-text text-3xl font-extrabold leading-normal text-transparent dark:bg-gradient-to-l sm:text-4xl sm:leading-10 md:text-6xl">
+            Hey there! Discover Me and My Blogs.
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
